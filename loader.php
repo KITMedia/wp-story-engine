324225d5900cca207a69681f025b98612ebab049
Andreas Ek <andreas@elseif.se>
2018-04-29 17:59:48 +0200

    Bump up version

--- loader.php
+++ loader.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Story Engine WebHook
  * Description: Provide WordPress with an endpoint to receive content from Story Engine.
- * Version: 0.3
+ * Version: 0.4
  * Author: KITMedia
  */
 
